a6987fd5f82cb1b0b9b927799034f62622d589f2
judiebig <870869788@qq.com>
2025-09-02 20:42:52 +0800

    delete irrelevant code

---
+++ run.sh
@@ -0,0 +1,19 @@
+python main.py --run_time 10 --dataset ba_2motifs --method size_cd
+python main.py --run_time 10 --dataset mr --method size_cd
+python main.py --run_time 10 --dataset mutag --method size_cd
+python main.py --run_time 10 --dataset benzene --method size_cd
+
+python main.py --run_time 10 --dataset ba_2motifs --method att_cd
+python main.py --run_time 10 --dataset mr --method att_cd
+python main.py --run_time 10 --dataset mutag --method att_cd
+python main.py --run_time 10 --dataset benzene --method att_cd
+
+python main.py --run_time 10 --dataset ba_2motifs --method gsat_cd
+python main.py --run_time 10 --dataset mr --method gsat_cd
+python main.py --run_time 10 --dataset mutag --method gsat_cd
+python main.py --run_time 10 --dataset benzene --method gsat_cd
+
+python main.py --run_time 10 --dataset ba_2motifs --method cal_cd
+python main.py --run_time 10 --dataset mr --method cal_cd
+python main.py --run_time 10 --dataset mutag --method cal_cd
+python main.py --run_time 10 --dataset benzene --method cal_cd
